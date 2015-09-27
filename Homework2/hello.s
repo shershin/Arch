bfd97b5f644f5f13571b8f62df7f155ec54b0e26
shershin <michael.shershin@gmail.com>
2015-09-26 21:39:02 -0400

    ?

--- Homework2/hello.s
+++ Homework2/hello.s
@@ -14,9 +14,9 @@
 main:	.globl main
 	li $v0, 4		# $system call code for print string
 	la $a0, hello		# $address of string to print
-	syscall			# print the string    
+	syscall			# print the string
 exit:	li $v0, 10		# exit
 	syscall
-	
+
 	.data
 hello:	.asciiz "Hello, world!\n"
