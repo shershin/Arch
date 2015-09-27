712f557680bcf70488b7584ffbeac7afd41339fd
shershin <michael.shershin@gmail.com>
2015-09-26 20:59:16 -0400

    added some work

--- Homework2/factorial.s
+++
@@ -1,7 +0,0 @@
-# Michael Shershin
-# 200-49-739
-# CMPT 422L 111
-
-  .text
-main: .globl main
-  
--- Homework2/fibonacci.s
+++ Homework2/fibonacci.s
@@ -4,4 +4,11 @@
 
   .text
 main: .globl main
-  
+  li $v0, 4 #call for print
+  la $a0, prompt #bring up the data to be printed
+  syscall  #print it
+exit: li $v0, 10 #exit
+  syscall
+
+  .data
+prompt: .asciiz "Please enter a number for how long you want the fibonacci sequence to run.\n"
