29a803f6d035726b8e08f25d2cf1f3989e0ed0a4
shershin <michael.shershin@gmail.com>
2015-10-04 14:18:04 -0400

    made a bit of progress

--- Homework3/factoral.s
+++ Homework3/factoral.s
@@ -4,51 +4,40 @@
 
   .text
 main: .globl main
-  li $v0, 4 #call for print
-  la $a0, prompt #bring up the data to be printed
-  syscall  #print it
+  li $v0, 4                               #call for print
+  la $a0, prompt                          #bring up the data to be printed
+  syscall                                 #print it
 
-  li $v0, 5 #call for reading int
+  li $v0, 5                               #call for reading int
   syscall
 
-  sw $v0, input #save the users input
+  sw $v0, input                           #save the users input
 
-  li $v0, 4
-  la $a0, run
-  syscall
+  li $v0, 4                               #call for print
+  la $a0, run                             #bring up data to be printed
+  syscall                                 #print
 
-  #init sequence
-  li $s0, 0
-  li $s1, 1
-  sw $s0, xmin2
-  sw $s1, xmin1
-
-  #init loop controls
-  li $s2, 0 #loop control
-  lw $s3, input #load input
+                                          #init loop controls
+  li $s1, 0                               #the changing input
+  lw $s2, input                           #load input
 
 loop:
-  li $a0, 0 #first number in the seq
-  li $s4, 0 #input = 1
-  ble $s2, $s4, dspval
-  li $a0, 1 #second number in the seq
-  li $s4, 1 #input = 2
-  ble $s2, $s4, dspval
+
+  bge $s3, $s4, dspval
   lw $s0, xmin2
   lw $s1, xmin1
   add $a0, $s1, $s0
   sw $s1, xmin2
   sw $a0, xmin1
 dspval:
-  li $v0, 1 #call for print int
+  li $v0, 1                               #call for print int
   syscall
   li $v0, 4
   la $a0, newln
   syscall
-  li $s5, 1 #incerment value
-  add $s2, $s5, $s2 #add one to loop control
-  lw $s3, input #load input
-  ble $s2, $s3, loop
+  li $s5, 1                               #incerment value
+  add $s2, $s5, $s2                       #add one to loop control
+  bgtz $s5, loop                          #check to see if input is down to 0 if not repeat
 
 exit: li $v0, 10 #exit
   syscall
@@ -56,7 +45,8 @@ exit: li $v0, 10 #exit
   .data
 prompt: .asciiz "Please enter a number for how long you want the fibonacci sequence to run.\n"
 input: .word 1
-xmin1: .word 4
-xmin2: .word 4
+first: .word 4
+sec: .word 4
 newln: .asciiz "\n"
+multiply: .asciiz "x"
 run: .asciiz "Running:\n"
