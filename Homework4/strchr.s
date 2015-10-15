47cdf917deb2e32100b0066450f7507cfca95e8c
shershin <michael.shershin@gmail.com>
2015-10-14 20:44:00 -0400

    homework 4 done

--- Homework4/strchr.s
+++ Homework4/strchr.s
@@ -7,91 +7,97 @@ main: .globl main
   li $v0, 4                               #call for print
   la $a0, prompt                          #bring up the data to be printed
   syscall                                 #print it
+  
+  li $v0, 9				  #get the heap
+  li $a0, 256				  #set how many bytes are in the heap
+  syscall
+  
+  bnez $v0, heapgood			  #branch if heap valid
+  
+  li $v0, 4
+  la $a0, heaperror        		  #heap error message = bad heap
+  syscall
+  
+  j exit				  #get to the exit
+  
+heapgood:
+  move $s0, $v0				  #save heap address
+  move $a0, $v0				  #read into heap address
 
   li $v0, 8                               #call for reading string
-  la $a0, string			  #
-  li $a1, 256				  #load the string length
+  li $a1, 256				  #set the length +1 of the read	
   syscall
-
-  sw $a0, string                          #save the users input
-  sw $a1, length			  #save the strings length
-
-  beqz $a1, stringerror 
-  
   
   li $v0, 4                               #call for print
-  la $a0, charprompt                   	  #bring up data to be printed
+  la $a0, charprompt             	  #bring up data to be printed
   syscall                                 #print
   
-  
-  li $v0, 8				  #call for reading char
-  la $a0, char
-  li $a1, 1
+  li $v0, 12				  #call for reading char
   syscall
   
-  sw $a0, char
-  sw $a1, charlength
+  move $s1, $v0				  #save char
   
-  beqz $a1, charerror
+  move $a0, $s0				  #setting up string for strchr
+  move $a1, $s1				  #setting up char for strchr
+  jal strchr				  #call search function
   
+  bnez $v0, charfound                     #return if character is found
   
-  lw $a0, string                          #load input
-  li $s0, 0				  #loop control
-  jal loopcheck
-
+  li $v0, 4                               #call for print
+  la $a0, charerror                       #bring up the data to be printed
+  syscall                                 #print it
+  j exit
+  
+charfound:
+  subu $s2, $v0, $s0			  #return from strchr minus start of string
+  li $v0, 4                               #call for print
+  la $a0, charinstring                    #bring up the data to be printed
+  syscall                                 #print it
+  
+  move $a0, $s2				  #char location
+  li $v0, 1				  #print int
+  syscall
+  
 exit: li $v0, 10                          #exit
   syscall
 
 ##########################################
-
+# strchr function
+# $a0 = pointer to the string
+# $a1 = character to find in string
+# $v0 = return reg
+# $sp = stack
+# $ra = linkage 
 ##########################################
-stringerror:
- li $v0, 4
- la $v0, errorstring
- syscall
+strchr:
+  subu $sp, $sp 28    			  #push stack
+  sw $ra, 16($sp)                         #save linkage reg
+  sw $s0, 20($sp)			  #save work reg1
+  sw $s1, 24($sp)			  #save work reg2
+  li $v0, 0				  #set error return
+  beqz $a0, return			  #get out if null string
+
+  move $s0, $a0				  #save pointer to string
+loop:
+  lbu $s1, 0($s0)			  #get byte
+  beqz $s1, return			  #return if char is not found
+  beq $s1, $a1, found			  #if char is found jump
+  addiu $s0, $s0, 1			  #bump pointer by one byte 
+  j loop				  #jump back to loop
  
- jal exit
- 
-charerror:
- li $v0, 4
- la $v0, errorchar
- syscall
- 
- jal exit
- 
-notfound:
- li $v0, 4
- la $v0, founditisnot
- syscall
- 
- jal exit 
+found:
+  move $v0, $s0				  #set pointer to character
   
-foundit:
- li $v0, 4
- la $v0, found
- syscall
- 
- li $v0, 5
- syscall
- 
-loopcheck:
- 
- 
- 
-
- jr $ra
-
+return:
+  lw $ra, 16($sp)                         #restore linkage reg
+  lw $s0, 20($sp)			  #restore work reg1
+  lw $s1, 24($sp)			  #restore work reg2
+  addu $sp, $sp, 28                       #pop stack
+  jr $ra
 
   .data
 prompt:       .asciiz "Please enter a string.\n"
 charprompt:   .asciiz "Please enter a char to search for.\n"
-string:       .word 4
-char:         .word 4
-length:       .word 4
-charlength:   .word 4
-newln:        .asciiz "\n"
-error:        .asciiz "Value is too large, please run again but with a smaller number.\n"
-run:          .asciiz "Running:\n"
-errorstring:  .asciiz "Please enter a long enough string to check."
-errorchar:    .asciiz "Please enter a char."
-founditisnot: .asciiz "Found the char is not."
+heaperror:    .asciiz "No heap available.\n"
+charerror:    .asciiz "\nChar was not found in the string.\n"
+charinstring: .asciiz "\nThe char is loacated at "
